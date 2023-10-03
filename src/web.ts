27dbc8930afc129d9f228c49cf145f6d64d27ec6
Luke Thompson <luke@streamjar.tv>
2023-10-03 12:54:38 +0100

    fix: fix cors?

--- src/web.ts
+++ src/web.ts
@@ -26,7 +26,9 @@ const server: Server = new Server({
 	host: vaultConfig.get('web.host'),
 	port: vaultConfig.get('web.port'),
 	routes: {
-		cors: true,
+		cors: {
+			origin: ['https://sagl.app', 'https://sagl.mp']
+		},
 		validate: {
 			failAction: (_: Request, __: ResponseToolkit, err?: Error): Lifecycle.ReturnValue => {
 				throw err;
